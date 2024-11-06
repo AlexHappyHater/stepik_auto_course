04b77d58a039bfcdfcae99148ae91f707f703c53
AlexHappyHater <vissor93@yandex.ru>
2024-11-06 13:31:52 +0400

    upgrade all packages in requrements.txt

--- conftest.py
+++ conftest.py
@@ -2,12 +2,24 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default=None,
+                     help="Choose browser: chrome or firefox")
+
+
 @pytest.fixture(scope="function")
-def browser():
-    print("\nОТКРЫЛИ БРАУЗЕР")
-    browser=webdriver.Chrome()
-    browser.implicitly_wait(15)
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nЗАПУСКАЕМ ХРОМ")
+        browser = webdriver.Chrome()
+    elif browser_name == "firefox":
+        print("\nЗАПУСКАЕМ ОГНЕЛИСА")
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
     print("\nЗАКРЫЛИ БРАУЗЕР")
     browser.quit()
-
